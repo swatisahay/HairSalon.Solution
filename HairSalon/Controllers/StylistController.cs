f3b3ccf04f391c9aa0980913f2cd2abc5b3071c8
Swati Sahay <swatiranjan0111@gmail.com>
2018-07-20 18:48:59 -0700

    add view for stylist delete and edit

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -44,12 +44,27 @@ namespace HairSalon.Controllers
 
       return View(newStylist);
      }
-      // [HttpGet("/items/{id}/update")]
-      //    public ActionResult UpdateForm(int id)
-      //    {
-      //        Item thisItem = Item.Find(id);
-      //        return View(thisItem);
-      //    }
+     [HttpGet("/stylist/{id}/update")]
+     public ActionResult UpdateForm(int id)
+     {
+       Stylist thisStylist = Stylist.Find(id);
+       return View(thisStylist);
+     }
+     [HttpPost("/stylist/{id}/update")]
+     public ActionResult Update(int id)
+     {
+       Stylist thisStylist = Stylist.Find(id);
+       thisStylist.Edit(Request.Form["updatename"]);
+       return RedirectToAction("Index");
+     }
+
+     [HttpGet("/stylist/{id}/delete")]
+     public ActionResult Delete(int id)
+     {
+       Stylist thisStylist = Stylist.Find(id);
+       thisStylist.Delete();
+       return RedirectToAction("Index");
+     }
       [HttpGet("/stylist/{id}/clients")]
       public ActionResult Clientdetail(int id)
       {
